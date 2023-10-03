8951375040e6b259f3487fd64a63004ebb500240
unknown <jahirulislam41185@gmail.com>
2023-10-03 16:15:58 +0600

    hotel details card completed

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     const hotels = useSelector(state => state.hotels.hotels);
 
     return (
-        <div className="min-h-screen my-10 grid grid-cols-3 gap-8">
+        <div className="min-h-screen my-10 grid gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
             {hotels?.map(h => <HotelCard key={h._id} hotel={h}></HotelCard>)}
         </div>
     );
--- src/components/HotelCard.jsx
+++ src/components/HotelCard.jsx
@@ -1,13 +1,34 @@
 import { FaArrowsAlt, FaBath, FaBed, FaBuilding } from 'react-icons/fa';
+import { BiHeart } from "react-icons/bi";
+import { IoLocationOutline } from "react-icons/io5";
 
 const HotelCard = ({ hotel }) => {
-    const { hotelName, city, rating, currentLocation, room, bed, bath, flatSize, price, image, _id } = hotel;
+
+    const { hotelName, rating, currentLocation, condition, room, bed, bath, flatSize, price, image } = hotel;
 
     return (
         <div className="shadow-2xl rounded-2xl bg-[#F5F4FD]">
+            {/* condition and like button  */}
+            <div className='absolute'>
+                <button className='text-blue-700 font-bold bg-white
+                text-lg py-2 px-4 border capitalize rounded-3xl relative top-8 left-10'>
+                    {condition}</button>
+                <button className='relative top-8 left-96 bg-white rounded-full p-3 text-2xl text-blue-700'><BiHeart></BiHeart></button>
+            </div>
+             {/* hotel images  */}
             <img className="w-full h-96 p-4 rounded-3xl" src={image} alt="hotel image" />
-           
+
+             {/* popular button  */}
+            <div className='absolute'>
+                {rating < 4.5 && <button className='px-4 py-2 bg-[#363AE3] text-white font-semibold text-lg rounded-r relative bottom-10'>popular</button>}
+            </div>
+
+             {/* hotel details  */}
             <div className="px-6 py-4">
+                <div className='flex items-center space-x-2 my-2'>
+                    <IoLocationOutline className='text-xl'></IoLocationOutline>
+                    <span className='font-semibold'>{currentLocation}</span>
+                </div>
                 <div className="font-bold text-2xl mb-6">{hotelName}</div>
                 <div className='flex justify-between items-center mx-6'>
                     <div className='flex flex-col items-center'>
@@ -30,7 +51,7 @@ const HotelCard = ({ hotel }) => {
             </div>
             <div className="px-6 py-6 flex justify-between">
                 <p>
-                    <span className='text-[#363AE3] text-3xl font-semibold'>{price}</span>
+                    <span className='text-[#363AE3] text-3xl font-bold'>${price}</span>
                     <span className='text-2xl font-semibold'>/month</span>
                 </p>
                 <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-bold 
